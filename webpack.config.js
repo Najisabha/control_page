b3dd44efe44963567fa464b682c71bfee1491f56
naji sabha <nageammar628@gmail.com>
2025-04-02 17:11:40 +0300

    V1.0

--- webpack.config.js
+++ webpack.config.js
@@ -1,65 +1,96 @@
 const HtmlWebpackPlugin = require("html-webpack-plugin");
-
-const path = require('path');
-
+const path = require("path");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const OptimizeCssAssetsPlugin = require("optimize-css-assets-webpack-plugin");
 module.exports = {
-
-    entry: './src/index.js',
-
+    mode: "development",
+    entry: "./src/index.js",
     output: {
-
-        path: path.resolve(__dirname, '/app'),
-
-        filename: 'app.js'
-
+        path: path.resolve(__dirname, "app"), // ✅ تصحيح المسار
+        filename: "app.js"
     },
-
     module: {
-
         rules: [
-
-          {
-
-            test: /\.html$/i,
-
-            loader: 'html-loader',
-
-          },
+            {
+                test: /\.html$/i,
+                loader: "html-loader",
+            },
+            {
+                test: /\.(sa|sc|c)ss$/i,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    "css-loader",
+                    {
+                        loader: "postcss-loader",
+                        options: {
+                            postcssOptions: {
+                                plugins: [
+                                    require("autoprefixer"),
+                                ],
+                            },
+                        },
+                    },
+                    "sass-loader",
+                ],
+            },
+            {
+                test: /\.(png|jpe?g|gif|svg)$/i,
+                type: 'asset/resource',
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/i,
+                type: 'asset/resource',
+            },
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader",
+                    options: {
+                        presets: ["@babel/preset-env"]
+                    }
+                }
+            },
         ]
-
     },
-
+    devServer: {
+        static: path.join(__dirname, "app"), // ✅ استبدال contentBase بـ static
+        compress: true,
+        port: 8081,
+        open: true,
+        devMiddleware: {
+            writeToDisk: true // ✅ إصلاح writeToDisk
+        }
+    },
     plugins: [
-        new HtmlWebpackPlugin({
-            filename: 'index.html',
-            template: './src/index.html',
-
+        new CleanWebpackPlugin(),
+        new MiniCssExtractPlugin({
+            filename: "assets/css/style.css",
         }),
         new HtmlWebpackPlugin({
-            filename: 'orders.html',
-            template: './src/orders.html',
-
+            filename: "index.html",
+            template: "./src/index.html",
         }),
-        new HtmlWebpackPlugin({
-            filename: 'products.html',
-            template: './src/products.html',
-
-        }),
-        new HtmlWebpackPlugin({
-            filename: 'user.html',
-            template: './src/user.html',
-
-        }),
-        new HtmlWebpackPlugin({
-            filename: 'add-product.html',
-            template: './src/add-product.html',
-
-        }),
-        new HtmlWebpackPlugin({
-            filename: 'add-user.html',
-            template: './src/add-user.html',
-
-        }),
+        // new HtmlWebpackPlugin({
+        //     filename: "orders.html",
+        //     template: "./src/orders.html",
+        // }),
+        // new HtmlWebpackPlugin({
+        //     filename: "products.html",
+        //     template: "./src/products.html",
+        // }),
+        // new HtmlWebpackPlugin({
+        //     filename: "user.html",
+        //     template: "./src/user.html",
+        // }),
+        // new HtmlWebpackPlugin({
+        //     filename: "add-product.html",
+        //     template: "./src/add-product.html",
+        // }),
+        // new HtmlWebpackPlugin({
+        //     filename: "add-user.html",
+        //     template: "./src/add-user.html",
+        // }),
     ]
-
-}
+};
